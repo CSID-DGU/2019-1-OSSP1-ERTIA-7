d8aacebb021def55ac2df0274d3f51eeec9ebcd5
biny <kbpark829@gmail.com>
2019-05-04 18:35:03 +0900

    for revert

--- TrashBin-master/TrashBin/be/app.js
+++ TrashBin-master/TrashBin/be/app.js
@@ -4,10 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/api/index');
+var usersRouter = require('./routes/api/users');
 
 var app = express();
+
+const history = require('connect-history-api-fallback')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
