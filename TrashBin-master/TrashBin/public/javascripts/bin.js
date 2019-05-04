1dd5973ebb38a019cae687604a8b36de0834c4fe
biny <kbpark829@gmail.com>
2019-05-04 13:54:55 +0900

    change ejs to vue

--- TrashBin-master/TrashBin/public/javascripts/bin.js
+++
@@ -1,6 +0,0 @@
-var btnAddBin = document.getElementById('btnAddBin');
-var divBinForm = document.getElementById('divBinForm');
-
-btnAddBin.addEventListener('click', function(event) {
-    divBinForm.setAttribute('style', 'display: in-block; visibility: visible;');
-});
