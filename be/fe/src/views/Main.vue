3de55359bf0e6dad2cc0c9bd91b8368b6659195a
biny <kbpark829@gmail.com>
2019-05-12 01:42:28 +0900

    add submit done

--- be/fe/src/views/Main.vue
+++ be/fe/src/views/Main.vue
@@ -1,42 +1,250 @@
 <template>
   <v-layout row wrap>
-    <v-flex xs12 sm12>
-        <v-btn large color="teal" style="color:white">add bin</v-btn>
-    </v-flex>
+<v-snackbar
+      v-model="snackbar"
+      absolute
+      top
+      right
+      color="success"
+    >
+      <span>성공적으로 추가되었습니다!</span>
+      <v-icon dark>check_circle</v-icon>
+  </v-snackbar>
+<!-- 쓰레기통 추가 dialog -->
+   <v-dialog v-model="addDL" persistent max-width="600px">
+     <template v-slot:activator="{ on }">
+       <v-flex xs12 sm12>
+       <v-btn color="teal" style="font-weight: bold" dark  v-on="on">쓰레기통 추가</v-btn>
+       </v-flex>
+     </template>
+     <v-form ref="form">
+     <v-card>
+       <v-card-title>
+         <span class="headline">쓰레기통 정보</span>
+       </v-card-title>
+       <v-card-text>
+         <v-container grid-list-md>
+           <v-layout wrap>
+             <v-flex xs12 sm12>
+               <v-text-field 
+               v-model="newBT.title"
+               label="쓰레기통 이름" 
+               required></v-text-field>
+             </v-flex>
+             <v-flex xs12 sm12>
+               <v-select
+                v-model="newBT.district"
+                 :items="['강남구', '강동구', '강북구', '강서구', '관악구', '광진구', '구로구', '금천구', '노원구', '도봉구', '동대문구', '동작구', '마포구', '서대문구', '서초구', '성동구', '성북구', '송파구', '양천구', '영등포구', '용산구', '은평구', '종로구', '중구', '중랑구']"
+                 label="쓰레기통 위치"
+                 required
+               ></v-select>
+             </v-flex>
+             <v-flex xs6 sm6>
+               <v-text-field 
+               v-model="newBT.lat"
+               ref = "trashLat" 
+               label="좌표 위도" 
+               required></v-text-field>
+             </v-flex>
+             <v-flex xs6 sm6>
+               <v-text-field 
+               v-model="newBT.long"
+               ref = "trashLng" 
+               label="좌표 경도" 
+               required></v-text-field>
+             </v-flex>
+             <!-- <GmapMap
+                :center="{lat:37.555, lng:127}"
+                :zoom="11.5"
+                map-type-id="terrain"
+                style="width: 500px; height: 200px"
+                    >
+              <GmapMarker
 
+
+                :key="index"
+                v-for="(m, index) in markers"
+                :position="m.position"
+                :clickable="true"
+                :draggable="true"
+                @click="center=m.position"
+              />
+            </GmapMap> -->
+           </v-layout>
+         </v-container>
+       </v-card-text>
+       <v-card-actions>
+         <v-spacer></v-spacer>
+         <v-btn style="font-weight: bold" color="blue darken-1" flat @click="addSubmit">저장</v-btn>
+         <v-btn style="font-weight: bold" flat @click="addDL = false">취소</v-btn>
+       </v-card-actions>
+     </v-card>
+     </v-form>
+   </v-dialog>
+
+
+<!-- 각각 쓰레기통 카드들 -->
     <v-flex xs2 sm2
-    v-for="card in cards"
-    :key="card.title"
+    v-for="trashbin in trashbins"
+    :key="trashbin.title"
     class="ml-2">
       <v-card >
         <v-card-title primary-title>
           <div>
-            <h3 class="headline mb-0">{{card.title}}</h3>
+            <h3 class="headline mb-0">{{trashbin.title}}&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp{{trashbin.amount}}%</h3>
           </div>
         </v-card-title>
         <div>
-          <h4 class="ml-3 mb-0">위치: {{card.location}}</h4>
+          <h4 class="ml-3 mb-0">위치: {{trashbin.district}}</h4>
         </div>
 
         <v-card-actions>
-          <v-btn flat color="blue">edit bin</v-btn>
-          <v-btn flat color="red">delete bin</v-btn>
+          <v-dialog v-model="editDL" persistent max-width="600px">
+
+
+<!-- 쓰레기통 수정 버튼 dialog-->
+            <template v-slot:activator="{ on }">
+              <v-flex xs12 sm12>
+              <v-btn flat color="blue" style="font-weight: bold" dark v-on="on">쓰레기통 수정</v-btn>
+              </v-flex>
+            </template>
+            <v-card>
+              <v-card-title>
+                <span class="headline">쓰레기통 정보</span>
+              </v-card-title>
+              <v-card-text>
+                <v-container grid-list-md>
+                  <v-layout wrap>
+                    <v-flex xs12 sm12>
+                      <v-text-field label="쓰레기통 이름" required></v-text-field>
+                    </v-flex>
+                    <v-flex xs12 sm12>
+                      <v-select
+                        :items="['강남구', '강동구', '강북구', '강서구', '관악구', '광진구', '구로구', '금천구', '노원구', '도봉구', '동대문구', '동작구', '마포구', '서대문구', '서초구', '성동구', '성북구', '송파구', '양천구', '영등포구', '용산구', '은평구', '종로구', '중구', '중랑구']"
+                        label="쓰레기통 위치"
+                        required
+                      ></v-select>
+                    </v-flex>
+                    <v-flex xs6 sm6>
+                      <v-text-field label="좌표 위도" required></v-text-field>
+                    </v-flex>
+                    <v-flex xs6 sm6>
+                      <v-text-field label="좌표 경도" required></v-text-field>
+                    </v-flex>
+                    <!-- <GmapMap
+                       :center="{lat:37.555, lng:127}"
+                       :zoom="11.5"
+                       map-type-id="terrain"
+                       style="width: 500px; height: 200px"
+                           >
+                     <GmapMarker
+
+
+                       :key="index"
+                       v-for="(m, index) in markers"
+                       :position="m.position"
+                       :clickable="true"
+                       :draggable="true"
+                       @click="center=m.position"
+                     />
+                   </GmapMap> -->
+                  </v-layout>
+                </v-container>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn style="font-weight: bold" color="blue darken-1" flat @click="editDL = false">저장</v-btn>
+                <v-btn style="font-weight: bold" flat @click="editDL = false">취소</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+
+<!-- 쓰레기통 삭제 -->
+            <v-dialog v-model="delDL" persistent max-width="600px">
+              <template v-slot:activator="{ on }">
+                <v-flex xs12 sm12>
+                <v-btn flat color="red" style="font-weight: bold" dark v-on="on">쓰레기통 삭제</v-btn>
+                </v-flex>
+              </template>
+              <v-card>
+                <v-card-title>
+                  <span class="headline">정말 삭제하시겠습니까?</span>
+                </v-card-title>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                    <v-btn color="red" style="font-weight: bold" flat @click="delDL = false">삭제</v-btn>
+                  <v-btn style="font-weight: bold" flat @click="delDL = false">취소</v-btn>
+                  <v-btn style="font-weight: bold" flat @click="delDL = false">취소</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
         </v-card-actions>
       </v-card>
     </v-flex>
   </v-layout>
+
+
 </template>
 
 <script>
+  import axios from 'axios'
+  import firebase from 'firebase'
+
+  let config = {
+    apiKey: "AIzaSyC_PXtAD4_mHIZDvBHwlilhY-c_AN3B0qY",
+    authDomain: "ertia-1555997688215.firebaseapp.com",
+    databaseURL: "https://ertia-1555997688215.firebaseio.com",
+    storageBucket: "gs://ertia-1555997688215.appspot.com/",
+  }
+
+  let fapp = firebase.initializeApp(config);
+  let db = fapp.database();
+
+  let tbsRef=db.ref('trashBins')
+
   export default {
     data () {
       return {
-        cards:[
-          {title: 'bin1', location: '중랑구'},
-          {title: 'bin2', location: '중구'},
-          {title: 'bin3', location: '동대문구'}
-        ]
+        newBT: {
+          title:'',
+          district:'',
+          lat:'',
+          long:'',
+          amount:''
+        },
+        //trashbins: [],
+        addDL: false,
+        editDL : false,
+        delDL : false,
+        snackbar:false
         }
+    },
+    mounted () {
+      // axios.get('http://localhost:3000/api/trashbin')
+      //   .then((r) => {
+      //     this.trashbins = r.data.trashbins
+      //     console.log(r)
+      //   })
+      //   .catch((e) => {
+      //     console.error(e.message)
+      //   })
+        tbsRef.on('value', function(tbs) {
+          tbs.forEach(function (childtbs) {
+            var tb=childtbs.val()
+            console.log(tb)
+          })
+        })
+    },
+    firebase: {
+      trashbins: db.ref('trashBins'),
+    },
+    methods: {
+      addSubmit () {
+        tbsRef.push(this.newBT)
+        console.log(this.newBT)
+        this.addDL=false
+        this.snackbar=true
+      }
     }
   }
 </script>
