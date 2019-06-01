860d62cb2e3865dc25b50573387134f5779230de
minhyung <mpsmhck95@naver.com>
2019-06-01 15:20:12 +0900

    수정

--- trashuser/user/routes/index.js
+++ trashuser/user/routes/index.js
@@ -3,10 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home', { title: 'home' });
 });
-router.get('/home', function(req, res, next) {
-  res.render('home', { title: 'Express' });
+router.get('/index', function(req, res, next) {
+  res.render('index', { title: 'index' });
 });
 
 module.exports = router;
